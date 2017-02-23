b7622e4977cc265175fac7b9b685e3920c827282
Liam Mc Cormick <ljmc2000@gmail.com>
2017-02-23 10:36:18 +0000

    added prototypes

---
+++ labtest3.c
@@ -0,0 +1,21 @@
+/*
+Program requirements:
+Enter 5 tempretures in celcius and manupulate them to farenheit and kelvin
+
+Author: Liam McCormick
+Date: 23-02-17
+OS: Gentoo
+Kernel: Linux 4.10
+Compiler: GCC 4.9.4
+*/
+
+#include <stdio.h>
+
+//prototypes
+float convert_temp(float*);	//convert to farenheit and calculate average
+float in_Kelvin(float);	//convert from celcius to kelvin
+
+int main()
+{
+
+}//end main
