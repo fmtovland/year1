b909fa86f895abad66144dfee5293371a9c6f96a
Liam Mc Cormick <ljmc2000@gmail.com>
2017-03-16 10:55:32 +0000

    added lib_book, customer_rec and transaction

--- week21/templates.c
+++ week21/templates.c
@@ -13,3 +13,27 @@ struct stock_rec
 	int instock;		//number of items in stock
 };
 
+struct lib_book
+{
+	char isbn[14];	//isbn with extra slot for null character
+	char name[31];	//name of book
+	char author[26];//name of author
+	float price;	//what the book costs to replace
+};
+
+struct customer_rec
+{
+	unsigned int id_no;	//the customers unique id on the system
+	char name[26];		//the customers name
+	char address[46];	//the customers address
+	double balance;		//customers outstanding bill. negitive if company is in customers debt
+
+};
+
+struct transaction
+{
+	char type;	//the type of transaction
+	short date[3];	//the date of the transaction (dd/mm/yy)
+	float cost;	//the amount of money that changed hands
+
+};
