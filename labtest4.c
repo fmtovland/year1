6db068c2c019830ed62c314994f04467a7d9998b
Liam Mc Cormick <ljmc2000@gmail.com>
2017-04-06 10:37:38 +0100

    first commit

--- labtest4.c
+++ labtest4.c
@@ -1,5 +1,6 @@
 /*
-Program description:
+Program description: Write a function to enter the details of 2 passengers flying on the same flight and same
+date. Then Write a function to display the details of the passengers entered in part (b) above.
 
 Author: Liam McCormick
 Date: 06-04-98
@@ -10,11 +11,29 @@ Compiler: GCC 4.9.4
 
 #include <stdio.h>
 #include <stdlib.h>
-#define	...
+#define	NAMNO 15	//max characters in the firstname
+#define SURNO 25	//max characters in the surname
 
 //struct templates
+struct date	//hold a date
+{
+	int day;	//day of the month
+	char month[4];	//eg jan, feb or mar
+	int year;
+};
+
+struct passenger	//hold passenger info
+{
+	int flightno;		//flight number
+	struct date;		//date of departure
+	char surname[SURNO];	//surname
+	char firstname[NAMNO];	//firstname
+	int seatno;		//seat number
+};
 
 //prototypes
+void enter_psngr(struct passenger);	//enter passenger info
+void display_psngr(struct passenger);	//remove passenger info
 
 int main()
 {
