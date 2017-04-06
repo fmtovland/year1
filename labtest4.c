7a5faa9a4a3e31022b152e5bd91b4385f4bd3923
Liam Mc Cormick <ljmc2000@gmail.com>
2017-04-06 09:55:31 +0100

    added template for today's lab test

---
+++ labtest4.c
@@ -0,0 +1,21 @@
+/*
+Program description:
+
+Author: Liam McCormick
+Date: 06-04-98
+OS: Gentoo
+Kernel: 4.9.18
+Compiler: GCC 4.9.4
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define	...
+
+//struct templates (here due to previous compiler errors I have experienced)
+
+//prototypes
+
+int main()
+{
+}//end main
